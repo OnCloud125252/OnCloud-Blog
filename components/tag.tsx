112010019f20777138c02ab87b669b84ed7c3b94
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-09 02:39:59 +0800

    fix: color of selected tag badge

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import { slug } from "github-slugger";
+import { usePathname } from "next/navigation";
+
 import { badgeVariants } from "./ui/badge";
 
 
@@ -8,13 +12,18 @@ interface TagProps {
   current?: boolean;
   count?: number;
 }
+
 export function Tag({ tag, current, count }: TagProps) {
+  const pathname = usePathname();
+
   return (
     <Link
-      className={badgeVariants({
-        variant: current ? "default" : "secondary",
-        className: "no-underline rounded-md"
-      })}
+      className={
+        badgeVariants({
+          variant: current ? "default" : "secondary",
+          className: "no-underline rounded-md"
+        }) + (pathname === `/tags/${slug(tag)}` ? " bg-primary" : "")
+      }
       href={`/tags/${slug(tag)}`}
     >
       {tag} {count ? `(${count})` : null}
